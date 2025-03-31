c0c3f282fef29322c99b2d695741518b1fe19252
Tesals <akinluaolorunfunminiyi@gmail.com>
2025-03-31 22:38:12 +0100

    added bot instance for each user

--- routes/ebay.js
+++ routes/ebay.js
@@ -177,6 +177,8 @@ router.get('/callback/store', asyncHandler(async (req, res) => {
     state: state,
     processed: false
   });
+
+  const tokenData = await ebayAPI.exchangeCodeForToken(code);
   
   logger.info('Stored eBay authorization code in database for processing');
   
